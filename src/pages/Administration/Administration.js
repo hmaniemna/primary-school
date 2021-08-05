e6dec2ee06e1687c5c010e5f23fd1ee8a43dd4f7
saraoualha <79280791+saraoualha@users.noreply.github.com>
2021-08-05 16:29:36 +0100

    Sara

--- src/pages/Administration/Administration.js
+++ src/pages/Administration/Administration.js
@@ -4,13 +4,13 @@ import { Button} from 'semantic-ui-react'
 const Administration = () => (
     <div>
         <Button.Group widths='7'>
-            <Button>المعلومات الشّخصيّة</Button>
-            <Button>التصرف في المدرسين</Button>
-            <Button>التصرف في الأقسام</Button>
-            <Button>التّصرّف في المواد</Button>
-            <Button>تعيين المدرّسين</Button>
-            <Button>التّصرّف في جدول الأوقات</Button>
-            <Button> صندوق الأدوات</Button>
+            <Button><a href="/admin/infos">المعلومات الشّخصيّة</a></Button>
+            <Button><a href="/admin/teachermanagemnet">التصرف في المدرسين</a></Button>
+            <Button><a href="/admin/classmanagement">التصرف في الأقسام</a></Button>
+            <Button><a href="/admin/subjectmanagemnet">التّصرّف في المواد</a></Button>
+            <Button><a href="/admin/teacherrecru">تعيين المدرّسين</a></Button>
+            <Button><a href="/admin/timetablemanagemnet">التّصرّف في جدول الأوقات</a></Button>
+            <Button><a href="/admin/tools"> صندوق الأدوات</a></Button>
         </Button.Group> 
     </div>
 )
--- src/pages/Teacher/Teacher.js
+++ src/pages/Teacher/Teacher.js
@@ -4,11 +4,11 @@ import { Button} from 'semantic-ui-react'
 const Teacher = () => (
     <div>
         <Button.Group widths='5'>
-            <Button>المعلومات الشّخصيّة</Button>
-            <Button>التصرف في التلاميذ</Button>
-            <Button> الحضور</Button>
-            <Button>جدول أوقاتي</Button>
-            <Button>إحصائيّات</Button>
+            <Button><a href="/">المعلومات الشّخصيّة</a></Button>
+            <Button><a href="/">التصرف في التلاميذ</a></Button>
+            <Button><a href="/"> الحضور</a></Button>
+            <Button><a href="/">جدول أوقاتي</a></Button>
+            <Button><a href="/">إحصائيّات</a></Button>
         </Button.Group>
     </div>
 )
