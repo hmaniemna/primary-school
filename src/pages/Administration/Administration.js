de3654f7af779e3ec7f1cefe041787dda2ab4e37
saraoualha <79280791+saraoualha@users.noreply.github.com>
2021-08-05 14:39:46 +0100

    test

---
+++ src/pages/Administration/Administration.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Administration=()=>{
+    return (
+        <div class="five ui buttons">
+            <button class="ui button">Overview</button>
+            <button class="ui button">Specs</button>
+            <button class="ui button">Warranty</button>
+            <button class="ui button">Reviews</button>
+            <button class="ui button">Support</button>
+        </div>
+    );
+}   
+
+export default Administration;
