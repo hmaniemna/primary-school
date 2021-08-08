963187fa7f85970f5f44d9469da8172380a78135
saraoualha <79280791+saraoualha@users.noreply.github.com>
2021-08-08 15:04:08 +0100

    .

--- src/App.js
+++ src/App.js
@@ -20,14 +20,14 @@ import StudentMangement from './pages/Teacher/StudentMangement/StudentMangement'
 import Timetable from './pages/Teacher/Timetable/Timetable';
 import LoginAdmin from "./pages/Login/LoginAdmin";
 import LoginTeacher from "./pages/Login/LoginTeacher";
-import Sign from "./pages/Login/Sign";
+//import Sign from "./pages/Login/Sign";
 
 const App = () => {
     return (
     <div>
         <Router>
             <div>
-            <Sign/>
+            <Header/>
                 <Switch>
                     <Route path="/" exact component={Home} />
                     <Route path="/admin" exact component={Administration} />
